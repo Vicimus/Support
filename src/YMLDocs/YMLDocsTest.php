0d49bd6607800f41e47680bb108b7d09db66668d
Jordan Grieve <quintilex@gmail.com>
2016-08-15 18:34:14 -0400

    working on test

--- src/YMLDocs/YMLDocsTest.php
+++ src/YMLDocs/YMLDocsTest.php
@@ -2,7 +2,9 @@
 
 namespace Vicimus\Support\YMLDocs;
 
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * A phpunit test class to easily test your YMLDoc Documentation.
@@ -15,5 +17,34 @@ use PHPUnit\Framework\TestCase;
  */
 class YMLDocsTest extends TestCase
 {
-    //
+    /**
+     * Holds the Guzzle client
+     *
+     * @var \GuzzleHttp\Client
+     */
+    private $http;
+
+    /**
+     * Hold the defined schemas
+     *
+     * @var mixed[]
+     */
+    protected $schemas = [];
+
+    /**
+     * Set up the guzzle client
+     */
+    public function __construct($path)
+    {
+        parent::__construct();
+        $this->http = new Client([
+
+        ]);
+
+        $contents = file_get_contents($path);
+        $global = Yaml::parse($contents);
+        if (array_key_exists('schemaDefinitions', $global)) {
+            $this->schemas = $global['schemaDefinitions'];
+        }
+    }
 }
