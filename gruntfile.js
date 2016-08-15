b9c89a3294b02fc2fa32f10ddaf78ab4ca68456e
Jordan Grieve <quintilex@gmail.com>
2016-08-15 18:12:19 -0400

    init

---
+++ gruntfile.js
@@ -0,0 +1,68 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+
+  var phpFiles = [
+    'src/**/*.php',
+    'tests/**/*.php'
+  ];
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    phpcs: {
+      application: {
+          src: phpFiles
+      },
+      options: {
+            bin: 'vendor/bin/phpcs',
+            standard: 'vendor/vicimus/standard/VicimusStandard,PSR2',
+            encoding: 'utf-8',
+            verbose: false
+      },
+    },
+
+    phpunit: {
+        classes: {
+            dir: ''
+
+        },
+        options: {
+            bin: './vendor/bin/phpunit',
+            colors: true
+        }
+    },
+
+    phpmd: {
+      application: {
+        dir: 'src/'
+      },
+      options: {
+        rulesets: 'codesize,cleancode,unusedcode',
+        reportFormat: 'text',
+      }
+    },
+
+    watch: {
+
+      files: phpFiles.concat(['tests/**/*.php']),
+      tasks: ['phpmd', 'phpcs', 'phpunit']
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-phpcs');
+  grunt.loadNpmTasks('grunt-phpunit');
+  grunt.loadNpmTasks('grunt-phpmd');
+
+  grunt.registerTask('default', ['phpmd', 'phpcs', 'phpunit']);
+};
---
+++ src/YMLDocs/YMLDocsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Vicimus\Support\YMLDocs;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A phpunit test class to easily test your YMLDoc Documentation.
+ *
+ * This test will ensure:
+ *      - your listed endpoints behave as expected
+ *      - your schemas returned match the specification
+ *
+ * @author Jordan Grieve <jgrieve@vicimus.com>
+ */
+class YMLDocsTest extends TestCase
+{
+    //
+}
