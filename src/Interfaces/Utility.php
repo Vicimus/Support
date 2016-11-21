0a8ee5f3353f845a190013ae57efd7028bae6fc7
Jordan Grieve <quintilex@gmail.com>
2016-11-21 14:11:24 -0500

    first draft of Utility interface

---
+++ src/Interfaces/Utility.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Vicimus\Support\Interfaces\Utility;
+
+/**
+ * Represents a Utility that can be run
+ *
+ * @author Jordan
+ */
+interface Utility
+{
+    /**
+     * The name of the utility
+     *
+     * @return string
+     */
+    public function name();
+
+    /**
+     * A description of what this utility does
+     *
+     * @return string
+     */
+    public function description();
+
+    /**
+     * Called to execute the utility
+     *
+     * @return void
+     */
+    public function call();
+
+    /**
+     * Called once the utility has completed
+     *
+     * @return mixed
+     */
+    public function finish();
+}
