75813ae73ad5edfe88bce8568e41cfc7696771b6
Jordan Grieve <quintilex@gmail.com>
2016-12-09 15:34:20 -0500

    updated the CSVParser interface

--- src/Interfaces/CSVParser.php
+++ src/Interfaces/CSVParser.php
@@ -12,11 +12,12 @@ interface CSVParser
     /**
      * Parse a CSV file
      *
-     * @param string $file The file to parse
+     * @param string $file    The file to parse
+     * @param array  $options Any options to set
      *
      * @return CSVParser
      */
-    public function parse($file);
+    public function parse($file, array $options = array());
 
     /**
      * The rows that were parsed from the CSV
@@ -33,4 +34,28 @@ interface CSVParser
      * @return string[]
      */
     public function headers(array $headers);
+
+    /**
+     * Make an instance of the parser but dont parse
+     *
+     * @param string $file    The file to parse
+     * @param array  $options Any options to set
+     * 
+     * @return CSVParser
+     */
+    public function make($file, array $options = array());
+
+    /**
+     * Iterate over each row of the CSV, one at a time
+     *
+     * @return string[]
+     */
+    public function each();
+
+    /**
+     * Get the hash value of the file specified in the parser
+     *
+     * @return string
+     */
+    public function hash();
 }
