b49bd40e4a682582200e8e14e251cb09623d0647
Jordan Grieve <quintilex@gmail.com>
2017-01-04 14:43:28 -0500

    added toArray to Vehicle interface

--- src/Interfaces/Vehicle.php
+++ src/Interfaces/Vehicle.php
@@ -6,6 +6,12 @@ namespace Vicimus\Support\Interfaces;
  * Enforces common methods among vehicle classes
  */
 interface Vehicle
-{
-    //
+{   
+    /**
+     * Return an array of properties that can be used to represent the
+     * vehicle
+     *
+     * @return array
+     */
+    public function toArray();
 }
