ed0ec496f2677a88d471de735360a04e95d4231a
Jordan Grieve <quintilex@gmail.com>
2017-03-07 14:42:26 -0500

    added standard output

---
+++ src/Classes/StandardOutput.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Vicimus\Support\Classes;
+
+use Vicimus\Support\Interfaces\ConsoleOutput;
+
+/**
+ * Standard output with basic color coding
+ */
+class StandardOutput implements ConsoleOutput
+{
+    /**
+     * Output information (green text)
+     *
+     * @param string $output The info to output
+     *
+     * @return void
+     */
+    public function info($output)
+    {
+        $this->line('');
+        echo "\033[32m".$output."\033[0m".PHP_EOL;
+    }
+
+    /**
+     * Output an error (red text)
+     *
+     * @param string $output The error to output
+     *
+     * @return void
+     */
+    public function error($output)
+    {
+        $this->line('');
+        echo "\033[31m".$output."\033[0m".PHP_EOL;
+    }
+
+    /**
+     * Output a comment (yellow text)
+     *
+     * @param string $output The comment to output
+     *
+     * @return void
+     */
+    public function comment($output)
+    {
+        $this->line('');
+        echo "\033[1;34m".$output."\033[0m".PHP_EOL;
+    }
+
+    /**
+     * Output text (grey text)
+     *
+     * @param string $output The text to output
+     *
+     * @return void
+     */
+    public function line($output)
+    {
+        echo str_pad("\r".$output, 80);
+        if (!$output) {
+            echo "\r";   
+        }
+    }
+}
