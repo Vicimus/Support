76d85789abfe008ea3babd691b179d52639e39da
Vincent Turcotte <vturcotte@vicimus.com>
2017-09-15 11:24:32 -0400

    Move parser functions in Parser class.

--- src/Classes/Tools.php
+++ src/Classes/Tools.php
@@ -137,133 +137,6 @@ class Tools
     }
 
     /**
-     * Parse a string and return detected sale class.
-     *
-     * @param string $input The sale class input
-     *
-     * @return string|null
-     */
-    public static function parseVehicleSaleClass(string $input): ?string
-    {
-        foreach (Enums::vehicleSaleClassesPatterns() as $key => $pattern) {
-            if (preg_match($pattern, $input)) {
-                return $key;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Parse a string and return detected payment types.
-     *
-     * @param string $input The payment type input
-     *
-     * @return string|null
-     */
-    public static function parsePaymentType(string $input): ?string
-    {
-        foreach (Enums::paymentTypesPatterns() as $key => $pattern) {
-            if (preg_match($pattern, $input)) {
-                return $key;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Parse a string and return detected country.
-     *
-     * @param string $input The country input
-     *
-     * @return string|null
-     */
-    public static function parseCountry(string $input): ?string
-    {
-        foreach (Enums::countriesPatterns() as $key => $pattern) {
-            if (preg_match($pattern, $input)) {
-                return $key;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Parse a string and return detected language.
-     *
-     * @param string $input The language input
-     *
-     * @return string|null
-     */
-    public static function parseLanguage(string $input): ?string
-    {
-        foreach (Enums::languagesPatterns() as $key => $pattern) {
-            if (preg_match($pattern, $input)) {
-                return $key;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Parse a string and return detected timezone.
-     *
-     * @param string $input The timezone input
-     *
-     * @return string|null
-     */
-    public static function parseTimezone(string $input): ?string
-    {
-        foreach (Enums::timezonesPatterns() as $key => $pattern) {
-            if (preg_match($pattern, $input)) {
-                return $key;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Parse a string and return detected mileage unit.
-     *
-     * @param string $input The mileage unit input
-     *
-     * @return string|null
-     */
-    public static function parseMileageUnit(string $input): ?string
-    {
-        foreach (Enums::mileageUnitsPatterns() as $key => $pattern) {
-            if (preg_match($pattern, $input)) {
-                return $key;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Parse a string and return detected state.
-     *
-     * @param string $input   The state input
-     * @param string $country In which country
-     *
-     * @return null|string
-     */
-    public static function parseState(string $input, string $country): ?string
-    {
-        foreach (Enums::statesPatterns($country) as $key => $pattern) {
-            if (preg_match($pattern, $input)) {
-                return $key;
-            }
-        }
-
-        return null;
-    }
-
-    /**
      * Get country from state.
      *
      * @param string $input The state input
@@ -274,7 +147,7 @@ class Tools
     {
         $countries = array_keys(Enums::countries());
         foreach ($countries as $country) {
-            $state = Tools::parseState($input, $country);
+            $state = Parser::parseState($input, $country);
             if ($state) {
                 return $country;
             }
---
+++ tests/Unit/ParserTest.php
@@ -0,0 +1,178 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Vicimus\Support\Classes\Parser;
+
+/**
+ * Class ParserTest
+ *
+ * @package Vicimus\Support\Tests\Unit
+ */
+class ParserTest extends TestCase
+{
+    /**
+     * Test parse sale classes function.
+     *
+     * @return void
+     */
+    public function testParseVehicleSaleClass(): void
+    {
+        $result = Parser::parseVehicleSaleClass('used');
+        $this->assertEquals('used', $result);
+
+        $result = Parser::parseVehicleSaleClass('new');
+        $this->assertEquals('new', $result);
+
+        $result = Parser::parseVehicleSaleClass('usagee');
+        $this->assertEquals('used', $result);
+
+        $result = Parser::parseVehicleSaleClass('usagé');
+        $this->assertEquals('used', $result);
+
+        $result = Parser::parseVehicleSaleClass('usageefpowiej');
+        $this->assertNull($result);
+
+        $result = Parser::parseVehicleSaleClass('neuf');
+        $this->assertEquals('new', $result);
+    }
+
+    /**
+     * Test parse country function.
+     *
+     * @return void
+     */
+    public function testParseCountry(): void
+    {
+        $result = Parser::parseCountry('ca');
+        $this->assertEquals('CA', $result);
+
+        $result = Parser::parseCountry('Canada');
+        $this->assertEquals('CA', $result);
+
+        $result = Parser::parseCountry('USA');
+        $this->assertEquals('US', $result);
+
+        $result = Parser::parseCountry('US');
+        $this->assertEquals('US', $result);
+
+        $result = Parser::parseCountry('United-States');
+        $this->assertEquals('US', $result);
+
+        $result = Parser::parseCountry('United-States of America');
+        $this->assertEquals('US', $result);
+    }
+
+    /**
+     * Test parse language function.
+     *
+     * @return void
+     */
+    public function testParseLanguage(): void
+    {
+        $result = Parser::parseLanguage('en');
+        $this->assertEquals('en', $result);
+
+        $result = Parser::parseLanguage('fr');
+        $this->assertEquals('fr', $result);
+
+        $result = Parser::parseLanguage('english');
+        $this->assertEquals('en', $result);
+
+        $result = Parser::parseLanguage('français');
+        $this->assertEquals('fr', $result);
+
+        $result = Parser::parseLanguage('eng');
+        $this->assertEquals('en', $result);
+
+        $result = Parser::parseLanguage('fra');
+        $this->assertEquals('fr', $result);
+    }
+
+    /**
+     * Test parse mileage unit function.
+     *
+     * @return void
+     */
+    public function testParseMileageUnit(): void
+    {
+        $result = Parser::parseMileageUnit('km');
+        $this->assertEquals('km', $result);
+
+        $result = Parser::parseMileageUnit('mi');
+        $this->assertEquals('mi', $result);
+
+        $result = Parser::parseMileageUnit('miles');
+        $this->assertEquals('mi', $result);
+
+        $result = Parser::parseMileageUnit('kilometers');
+        $this->assertEquals('km', $result);
+
+        $result = Parser::parseMileageUnit('kilomètres');
+        $this->assertEquals('km', $result);
+    }
+
+    /**
+     * Test parse payment type function.
+     *
+     * @return void
+     */
+    public function testParsePaymentType(): void
+    {
+        $result = Parser::parsePaymentType('Cash');
+        $this->assertEquals('cash', $result);
+
+        $result = Parser::parsePaymentType('Lease');
+        $this->assertEquals('lease', $result);
+
+        $result = Parser::parsePaymentType('Financing');
+        $this->assertEquals('finance', $result);
+
+        $result = Parser::parsePaymentType('Comptant');
+        $this->assertEquals('cash', $result);
+
+        $result = Parser::parsePaymentType('location');
+        $this->assertEquals('lease', $result);
+
+        $result = Parser::parsePaymentType('Financement');
+        $this->assertEquals('finance', $result);
+    }
+
+    /**
+     * Test parse timezone function.
+     *
+     * @return void
+     */
+    public function testParseTimezone(): void
+    {
+        $result = Parser::parseTimezone('PST');
+        $this->assertEquals('PST', $result);
+
+        $result = Parser::parseTimezone('eastern');
+        $this->assertEquals('EST', $result);
+
+        $result = Parser::parseTimezone('Heure de l\'Atlantique');
+        $this->assertEquals('ADT', $result);
+
+        $result = Parser::parseTimezone('Mountain timezone');
+        $this->assertEquals('MST', $result);
+    }
+
+    /**
+     * Test parse state function.
+     *
+     * @return void
+     */
+    public function testParseState(): void
+    {
+        $result = Parser::parseState('Québec', 'CA');
+        $this->assertEquals('QC', $result);
+
+        $result = Parser::parseState('Ontario', 'CA');
+        $this->assertEquals('ON', $result);
+
+        $result = Parser::parseState('Alabama', 'US');
+        $this->assertEquals('AL', $result);
+    }
+}
