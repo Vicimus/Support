fc94ff0c9a5106a8966c4c8ccee4d29961d86962
Jordan Grieve <jgrieve@vicimus.com>
2018-01-25 16:04:36 -0500

    fix

--- src/Interfaces/Blocks/Block.php
+++ src/Interfaces/Blocks/Block.php
@@ -2,7 +2,9 @@
 
 namespace Vicimus\Support\Interfaces\Blocks;
 
+use Illuminate\Database\Eloquent\Model;
 use stdClass;
+use Vicimus\Eevee\Models\Block as BlockModel;
 
 /**
  * Interface Block
@@ -17,6 +19,15 @@ interface Block
     public function getLimit(): int;
 
     /**
+     * Get an eloquent model to represent this block
+     *
+     * @param BlockModel $model The raw model row
+     *
+     * @return Model
+     */
+    public function getModel(BlockModel $model): Model;
+
+    /**
      * Get the name of the block
      *
      * @return string
@@ -29,6 +40,20 @@ interface Block
      * @return string[]
      */
     public function getPageTypes(): array;
+
+    /**
+     * Path on the disk where this block layouts live
+     *
+     * @return string
+     */
+    public function layoutPath(): string;
+
+    /**
+     * Get available layout information
+     *
+     * @return mixed[]
+     */
+    public function layouts(): array;
 
     /**
      * Convert the instance into a json object
