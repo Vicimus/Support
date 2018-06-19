e44398680a0b4651a2e81ee5693bbe01c0d55367
Andrew Benner <abenner@vicimus.com>
2018-06-19 08:47:08 -0400

    Update 2pac script to add --chillin-in-cuba flag

--- scripts/2pac.sh
+++ scripts/2pac.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# A script to check in vendor/vicimus packages for changes
+# and tags that need to be pushed
+# Apply the --chillin-in-cuba flag to determine which
+# packages need a new tag
+
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
@@ -30,17 +36,26 @@ crawl () {
             changedDirs+=(${dir})
         fi
 
-        tags="$(git describe --tags | grep '-')"
-        if [ ! -z "$tags" ]
+        if [ "$2" == "--chillin-in-cuba" ]
         then
-            tagDirs+=(${dir})
+            tags="$(git checkout master &> /dev/null && git pull origin master &> /dev/null && git describe | grep '-')"
+            if [ ! -z "$tags" ]
+            then
+                tagDirs+=(${dir})
+            fi
+        else
+            tags="$(git describe --tags | grep '-')"
+            if [ ! -z "$tags" ]
+            then
+                tagDirs+=(${dir})
+            fi
         fi
 
         cd ../../..
     done
 }
 
-crawl 'vendor/vicimus/*'
+crawl 'vendor/vicimus/*' $1
 
 if [ "$1" == "--dealer" ]
 then
