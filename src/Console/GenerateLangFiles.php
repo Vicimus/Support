b6d3d34c86182c35007c24c01ed7e8445ab746e4
Jordan Grieve <quintilex@gmail.com>
2018-07-09 15:22:46 -0400

    lang generator

---
+++ src/Console/GenerateLangFiles.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Console;
+
+use Illuminate\Console\Command;
+use Vicimus\Support\Classes\StandardOutput;
+use Vicimus\Support\Locale\LangGenerator;
+
+/**
+ * Class GenerateLangFiles
+ */
+class GenerateLangFiles extends Command
+{
+    /**
+     * The signature
+     * @var string
+     */
+    protected $signature = 'locale:generate {locales=en}';
+
+    /**
+     * Generate lang files
+     *
+     * @param LangGenerator $generator The generator
+     */
+    public function handle(LangGenerator $generator): void
+    {
+        $locales = explode(',', $this->argument('locales'));
+        $generator->bind(new StandardOutput())->fire(app_path(), resource_path('lang'), $locales);
+    }
+}
---
+++ src/Locale/LangGenerator.php
@@ -0,0 +1,115 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Locale;
+
+use Symfony\Component\Finder\Finder;
+use Vicimus\Support\Exceptions\DuplicateTranslationException;
+use Vicimus\Support\Interfaces\ConsoleOutput;
+use Vicimus\Support\Traits\ConsoleOutputter;
+
+/**
+ * Class LangGenerator
+ */
+class LangGenerator implements ConsoleOutput
+{
+    use ConsoleOutputter;
+
+    /**
+     * Generate lang files
+     *
+     * @return void
+     */
+    public function fire(string $path, string $localePath, array $locales): void
+    {
+        $dictionary = [];
+
+        $finder = new Finder();
+        $finder->files()->in($path)->name('*.php')->contains('___(');
+        foreach ($finder as $file) {
+            $this->line('%s', $file->getRelativePathname());
+            $this->process($file->getRealPath(), $dictionary);
+        }
+
+        $this->info('Found %s keys', count($dictionary));
+
+        $this->writeFiles($localePath, $locales, $dictionary);
+    }
+
+    /**
+     * @param string $path
+     * @param array  $dictionary
+     */
+    private function process(string $path, array &$dictionary): void
+    {
+        $contents = file_get_contents($path);
+        $matches = [];
+        preg_match_all('/___\(.*\)/', $contents, $matches);
+        foreach ($matches[0] ?? [] as $match) {
+            $line = str_replace(['___(\'', '\'))', '\')',], '', $match);
+            [$key, $value] = explode(',', $line, 2);
+            $key = substr(trim($key), 0, -1);
+            $value = substr(trim($value), 1);
+            if (isset($dictionary[$key]) && $dictionary[$key] !== $value) {
+                throw new DuplicateTranslationException(sprintf(
+                    'The key [%s] was found more than once and it has different values. ' .
+                    'The first key is [%s], the second key was [%s]',
+                    $key,
+                    $dictionary[$key],
+                    $value
+                ));
+            }
+
+            $dictionary[$key] = $value;
+        }
+    }
+
+    /**
+     * Write a locale file
+     *
+     * @param string   $localePath The locale path
+     * @param string[] $locale     The locale
+     * @param string[] $dictionary The dictionary
+     */
+    private function writeFiles(string $localePath, array $locales, array $dictionary): void
+    {
+        $files = [];
+
+        foreach ($dictionary as $key => $value) {
+            $parts = explode('.', $key);
+            $arrayKey = array_splice($parts, -1, 1)[0];
+            $filePath = implode('/', $parts);
+
+            if (!isset($files[$filePath])) {
+                $files[$filePath] = [];
+            }
+
+            $files[$filePath][$arrayKey] = $value;
+        }
+
+        foreach ($locales as $locale) {
+            foreach ($files as $file => $values) {
+                $path = sprintf('%s/%s', $localePath, $locale);
+                if (!file_exists($path)) {
+                    mkdir($path);
+                }
+
+                $target = sprintf('%s/%s/%s.php', $localePath, $locale, $file);
+
+                if (file_exists($target)) {
+                    $existing = include $target;
+
+                    $values = array_merge($values, $existing);
+                }
+
+                ksort($values);
+
+                $output = '<?php declare(strict_types = 1);' . PHP_EOL . PHP_EOL;
+                $output .= 'return ';
+                $output .= str_replace('array (', '[', substr(var_export($values, true), 0, -1));
+                $output .= '];' . PHP_EOL;
+
+                file_put_contents($target, $output);
+            }
+        }
+    }
+}
