be3ad9132a701793c14a0825ea9bd4598d745b3e
Andrew Benner <abenner@vicimus.com>
2018-07-09 08:47:42 -0400

    Create helpers.php file for helper functions

---
+++ src/helpers.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Classes;
+
+/**
+ * Translate the given message.
+ *
+ * @param  string  $key     The translation file key
+ * @param  string  $default The default value
+ * @param  array   $replace The array of variables
+ * @param  string  $locale  The locale
+ *
+ * @return string|array|null
+ */
+function ___($key, $default, $replace = [], $locale = null)
+{
+    $result = __($key, $replace, $locale);
+    if ($result === $key) {
+        return $default;
+    }
+
+    return $result;
+}
