eabfbbbe1cb8c07891ce119634c23f4e9bf8da49
Jordan Grieve <quintilex@gmail.com>
2018-08-28 10:40:58 -0400

    script cache

---
+++ src/Console/CacheScripts.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Console;
+
+use Illuminate\Console\Command;
+use Vicimus\Support\FrontEnd\ScriptCache;
+
+/**
+ * Class CacheScripts
+ */
+class CacheScripts extends Command
+{
+    /**
+     * The script cache
+     *
+     * @var ScriptCache
+     */
+    protected $cache;
+
+    /**
+     * The description
+     *
+     * @var string
+     */
+    protected $description = 'Read and cache script names';
+
+    /**
+     * Command signature
+     *
+     * @var string
+     */
+    protected $signature = 'scripts:cache';
+
+    /**
+     * CacheScripts constructor
+     *
+     * @param ScriptCache $cache The script cache object
+     */
+    public function __construct(ScriptCache $cache)
+    {
+        parent::__construct();
+        $this->cache = $cache;
+    }
+
+    /**
+     * Fire the command
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $this->cache->forget();
+        $this->cache->cache();
+    }
+}
