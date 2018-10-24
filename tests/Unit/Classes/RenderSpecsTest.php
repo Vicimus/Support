a0aad8727a74c24571484f8f0cac6311dd145cfd
Jordan Grieve <quintilex@gmail.com>
2018-10-24 13:40:50 -0400

    coverage

---
+++ tests/Unit/Classes/RenderSpecsTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Tests\Unit\Classes;
+
+use Vicimus\Support\Classes\RenderSpecs;
+use Vicimus\Support\Testing\TestCase;
+
+/**
+ * Class RenderSpecsTest
+ */
+class RenderSpecsTest extends TestCase
+{
+    /**
+     * Constructor should assign properties properly
+     *
+     * @return void
+     */
+    public function testConstructor(): void
+    {
+        $render = new RenderSpecs(1, 2, 3, 4);
+        $this->assertEquals(1, $render->width);
+        $this->assertEquals(2, $render->height);
+        $this->assertEquals(3, $render->scale);
+        $this->assertEquals(4, $render->pages);
+    }
+}
