98c3a78ed70428c5074dc50de6ae9aa52d560148
Jordan Grieve <quintilex@gmail.com>
2018-10-24 13:14:21 -0400

    update to the interface for renderers

--- src/Interfaces/ScreenCapturer.php
+++ src/Interfaces/ScreenCapturer.php
@@ -19,11 +19,15 @@ interface ScreenCapturer
     /**
      * Convert a URL into a PDF representation of that web-page
      *
-     * @param string $url The URL to capture
+     * @param string $url    The URL to capture
+     * @param int    $width  The width of the entity or screen
+     * @param int    $height The height of the entity or screen
+     * @param int    $scale  The value to scale to
+     * @param int    $pages  The number of pages to limit to
      *
      * @return string
      */
-    public function pdf(string $url): string;
+    public function pdf(string $url, ?int $width, ?int $height, ?int $scale, ?int $pages): string;
 
     /**
      * Convert a URL into a PNG representation of that web-page
