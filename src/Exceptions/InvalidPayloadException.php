a950faf01ce0693b905b32b09b05238f9559c1c0
Jordan Grieve <quintilex@gmail.com>
2018-11-06 20:29:37 -0500

    fix lint

--- src/Exceptions/InvalidPayloadException.php
+++ src/Exceptions/InvalidPayloadException.php
@@ -4,6 +4,9 @@ namespace Vicimus\Support\Exceptions;
 
 use InvalidArgumentException as InvalidArgument;
 
+use function get_class;
+use function is_object;
+
 /**
  * Class InvalidArgumentException
  */
@@ -12,10 +15,11 @@ class InvalidPayloadException extends InvalidArgument
     /**
      * InvalidArgumentException constructor
      *
-     * @param mixed    $got           The argument that was invalid
-     * @param string[] ...$acceptable What would have been acceptable
+     * @param mixed          $got           The argument that was invalid
+     * @param string         $parameter     The parameter we want to describe
+     * @param string[]|mixed ...$acceptable What would have been acceptable
      */
-    public function __construct($got, string $parameter, string ...$acceptable)
+    public function __construct($got, string $parameter, ...$acceptable)
     {
         if ($parameter) {
             $parameter = sprintf(' for %s parameter', $parameter);
--- src/Testing/Application.php
+++ src/Testing/Application.php
@@ -4,156 +4,161 @@ namespace Vicimus\Support\Testing;
 
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Foundation\Application as LaravelApp;
+use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class Application
+ */
 class Application extends Container implements LaravelApp
 {
 
     /**
-     * Get the version number of the application.
-     *
-     * @return string
-     */
-    public function version()
-    {
-        // TODO: Implement version() method.
-    }
-
-    /**
      * Get the base path of the Laravel installation.
      *
-     * @return string
+     * @return string|mixed|void
      */
     public function basePath()
     {
-        // TODO: Implement basePath() method.
-    }
-
-    /**
-     * Get or check the current application environment.
-     *
-     * @return string
-     */
-    public function environment()
-    {
-        // TODO: Implement environment() method.
-    }
-
-    /**
-     * Determine if the application is running in the console.
-     *
-     * @return bool
-     */
-    public function runningInConsole()
-    {
-        // TODO: Implement runningInConsole() method.
-    }
-
-    /**
-     * Determine if the application is running unit tests.
-     *
-     * @return bool
-     */
-    public function runningUnitTests()
-    {
-        // TODO: Implement runningUnitTests() method.
-    }
-
-    /**
-     * Determine if the application is currently down for maintenance.
-     *
-     * @return bool
-     */
-    public function isDownForMaintenance()
-    {
-        // TODO: Implement isDownForMaintenance() method.
-    }
-
-    /**
-     * Register all of the configured providers.
-     *
-     * @return void
-     */
-    public function registerConfiguredProviders()
-    {
-        // TODO: Implement registerConfiguredProviders() method.
-    }
-
-    /**
-     * Register a service provider with the application.
-     *
-     * @param  \Illuminate\Support\ServiceProvider|string $provider
-     * @param  bool                                       $force
-     *
-     * @return \Illuminate\Support\ServiceProvider
-     */
-    public function register($provider, $force = false)
-    {
-        // TODO: Implement register() method.
-    }
-
-    /**
-     * Register a deferred provider and service.
-     *
-     * @param  string      $provider
-     * @param  string|null $service
-     *
-     * @return void
-     */
-    public function registerDeferredProvider($provider, $service = null)
-    {
-        // TODO: Implement registerDeferredProvider() method.
+        // Implement basePath() method.
     }
 
     /**
      * Boot the application's service providers.
      *
-     * @return void
+     * @return void|mixed
      */
     public function boot()
     {
-        // TODO: Implement boot() method.
-    }
-
-    /**
-     * Register a new boot listener.
-     *
-     * @param  mixed $callback
-     *
-     * @return void
-     */
-    public function booting($callback)
-    {
-        // TODO: Implement booting() method.
+        //  Implement boot() method.
     }
 
     /**
      * Register a new "booted" listener.
      *
-     * @param  mixed $callback
+     * @param mixed $callback The callback
      *
-     * @return void
+     * @return void|mixed
      */
     public function booted($callback)
     {
-        // TODO: Implement booted() method.
+        // Implement booted() method.
     }
 
     /**
-     * Get the path to the cached services.php file.
+     * Register a new boot listener.
      *
-     * @return string
+     * @param mixed $callback The callback
+     *
+     * @return void|mixed
      */
-    public function getCachedServicesPath()
+    public function booting($callback)
     {
-        // TODO: Implement getCachedServicesPath() method.
+        // Implement booting() method.
+    }
+
+    /**
+     * Get or check the current application environment.
+     *
+     * @return string|mixed
+     */
+    public function environment()
+    {
+        return 'testing';
     }
 
     /**
      * Get the path to the cached packages.php file.
      *
-     * @return string
+     * @return string|mixed|void
      */
     public function getCachedPackagesPath()
     {
-        // TODO: Implement getCachedPackagesPath() method.
-}}
+        // Implement getCachedPackagesPath() method.
+    }
+
+    /**
+     * Get the path to the cached services.php file.
+     *
+     * @return string|mixed|void
+     */
+    public function getCachedServicesPath()
+    {
+        // Implement getCachedServicesPath() method.
+    }
+
+    /**
+     * Determine if the application is currently down for maintenance.
+     *
+     * @return bool|mixed
+     */
+    public function isDownForMaintenance()
+    {
+        return false;
+    }
+
+    /**
+     * Register a service provider with the application.
+     *
+     * @param  ServiceProvider|string $provider The provider
+     * @param  bool|mixed             $force    Force
+     *
+     * @return ServiceProvider|mixed|void
+     */
+    public function register($provider, $force = false)
+    {
+        // Implement register() method when necessary
+    }
+
+    /**
+     * Register all of the configured providers.
+     *
+     * @return void|mixed
+     */
+    public function registerConfiguredProviders()
+    {
+        // Implement registerConfiguredProviders() method when necessary
+    }
+
+    /**
+     * Register a deferred provider and service.
+     *
+     * @param  string|mixed $provider The providers
+     * @param  string|null  $service  The service
+     *
+     * @return void|mixed
+     */
+    public function registerDeferredProvider($provider, $service = null)
+    {
+        // Implement registerDeferredProvider() method.
+    }
+
+    /**
+     * Determine if the application is running in the console.
+     *
+     * @return bool|mixed
+     */
+    public function runningInConsole()
+    {
+        return true;
+    }
+
+    /**
+     * Determine if the application is running unit tests.
+     *
+     * @return bool|mixed
+     */
+    public function runningUnitTests()
+    {
+        return true;
+    }
+
+    /**
+     * Get the version number of the application.
+     *
+     * @return string|mixed|void
+     */
+    public function version()
+    {
+        // Implement version
+    }
+}
