66621dd214712702b5aa95287cfce43472c24192
Jordan Grieve <quintilex@gmail.com>
2019-01-17 13:49:04 -0500

    equity

---
+++ src/Equity/ErrorCodes.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Equity;
+
+abstract class ErrorCodes
+{
+    public const UNSPECIFIED_RESULT = 2;
+    public const RETURNED_MULTIPLE = 3;
+    public const INVALID_PARAMS = 4;
+    public const NO_RESULTS = 5;
+}
