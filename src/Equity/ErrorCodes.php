a42b8da63d6072224504929be788d66dc8e6d1c8
Jordan Grieve <quintilex@gmail.com>
2019-01-18 15:50:00 -0500

    error codes

--- src/Equity/ErrorCodes.php
+++ src/Equity/ErrorCodes.php
@@ -2,11 +2,23 @@
 
 namespace Vicimus\Support\Equity;
 
+/**
+ * Error codes for equity
+ */
 abstract class ErrorCodes
 {
-    public const SUCCESS = 0;
-    public const UNSPECIFIED_RESULT = 2;
-    public const RETURNED_MULTIPLE = 3;
+    /** Invalid parameters were sent */
     public const INVALID_PARAMS = 4;
+
+    /** No results found for parameters */
     public const NO_RESULTS = 5;
+
+    /** Successful valuation */
+    public const SUCCESS = 0;
+
+    /** Unknown result */
+    public const UNSPECIFIED_RESULT = 2;
+
+    /** Returned multiple vehicles so no value */
+    public const RETURNED_MULTIPLE = 3;
 }
