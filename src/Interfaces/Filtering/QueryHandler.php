2700c45970f5bed92f7d8b84d3a58d635466e064
Jordan Grieve <quintilex@gmail.com>
2019-01-30 08:58:11 -0500

    another contract

---
+++ src/Interfaces/Filtering/QueryHandler.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces\FIltering;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Interface QueryHandler
+ */
+interface QueryHandler
+{
+    /**
+     * Pass the query to a handler
+     *
+     * @param Builder $query  The builder
+     * @param Filter  $filter The filter
+     * @param string  $type   The type of query being handled
+     *
+     * @return void
+     */
+    public function handle(Builder $query, Filter $filter, string $type): void;
+}
