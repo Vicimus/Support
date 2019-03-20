f86356cee7f8301754b31906085abfcb37273934
Jordan Grieve <quintilex@gmail.com>
2019-03-20 13:25:09 -0400

    utility

---
+++ src/Traits/UtilityFlag.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Vicimus\Support\Traits;
+
+/**
+ * Trait for adding flags to a utilities command.
+ */
+trait UtilityFlag
+{
+    /**
+     * Add flags to a utility
+     *
+     * Used by the utility manager to allow specific flags to be triggered when
+     * the command is run.
+     *
+     * @param string[]|mixed $flags The desired flags to add.
+     *
+     * @return string
+     */
+    public function addFlags($flags)
+    {
+        return view('utility.partials.flags')
+            ->with([
+                'flags' => $flags,
+            ])->render();
+    }
+}
