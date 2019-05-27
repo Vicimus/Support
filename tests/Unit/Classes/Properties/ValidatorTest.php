68a946c22f880ae780919adc9371a66f9e336f7a
Rob Houston <rhouston@vicimus.com>
2019-05-27 10:46:58 -0400

    coveragE

--- tests/Unit/Classes/Properties/ValidatorTest.php
+++ tests/Unit/Classes/Properties/ValidatorTest.php
@@ -2,6 +2,8 @@
 
 namespace Vicimus\Support\Tests\Unit\Classes;
 
+use Illuminate\Contracts\Validation\Validator as IllValidator;
+use Illuminate\Support\Collection;
 use Illuminate\Validation\Factory;
 use Vicimus\Support\Classes\Properties\Validator;
 use Vicimus\Support\Exceptions\RestException;
@@ -20,7 +22,7 @@ class ValidatorTest extends TestCase
      * @return void
      * @throws RestException
      */
-    public function testValidate(): void
+    public function testValidateInvalid(): void
     {
         $providerProp = $this->basicMock(Property::class);
         $providerProp->method('property')->willReturn('title');
@@ -32,7 +34,43 @@ class ValidatorTest extends TestCase
         $property = $this->basicMock(PropertyRecord::class);
         $property->method('name')->willReturn('title');
 
+        $validator = $this->basicMock(IllValidator::class);
+        $validator->method('fails')->willReturn(true);
+        $validator->method('errors')->willReturn(new Collection('title is required'));
+
         $factory = $this->basicMock(Factory::class);
+        $factory->method('make')->willReturn($validator);
+        $service = new Validator($factory);
+
+        try {
+            $service->validate($provider, $property, 'test');
+            $this->fail('Validator exception not thrown');
+        } catch (RestException $ex) {
+            $this->assertSame(422, $ex->getCode());
+        }
+    }
+    /**
+     * Assert validate
+     * @return void
+     * @throws RestException
+     */
+    public function testValidateValid(): void
+    {
+        $providerProp = $this->basicMock(Property::class);
+        $providerProp->method('property')->willReturn('title');
+        $providerProp->method('restrictions')->willReturn('required');
+
+        $provider = $this->basicMock(PropertyProvider::class);
+        $provider->method('properties')->willReturn([$providerProp]);
+
+        $property = $this->basicMock(PropertyRecord::class);
+        $property->method('name')->willReturn('title');
+
+        $validator = $this->basicMock(IllValidator::class);
+        $validator->method('fails')->willReturn(false);
+
+        $factory = $this->basicMock(Factory::class);
+        $factory->method('make')->willReturn($validator);
         $service = new Validator($factory);
 
         $this->assertTrue($service->validate($provider, $property, 'test'));
