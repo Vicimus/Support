4df964f9ded4dfa3e10949b95d7dbf9a61b276eb
Jordan Grieve <quintilex@gmail.com>
2019-05-28 16:09:47 -0400

    repository changes

--- src/Classes/ConquestCompatibilityMatrix.php
+++ src/Classes/ConquestCompatibilityMatrix.php
@@ -3,6 +3,10 @@
 namespace Vicimus\Support\Classes;
 
 use Illuminate\Contracts\View\View;
+use ReflectionClass;
+use ReflectionException;
+use Vicimus\Support\Interfaces\MarketingSuite\ConquestDataSource;
+use Vicimus\Support\Interfaces\MarketingSuite\ConquestDataSourceRepository;
 
 /**
  * Class ConquestCompatibilityMatrix
@@ -40,6 +44,9 @@ class ConquestCompatibilityMatrix extends ImmutableObject
     {
         $rows = [];
         foreach ($matrix as $source => $description) {
+            if (!$this->isValidSource($source)) {
+                continue;
+            }
             if ($description instanceof View) {
                 $description = $description->render();
             }
@@ -58,5 +65,32 @@ class ConquestCompatibilityMatrix extends ImmutableObject
     public function toArray(): array
     {
         return $this->attributes['matrix'];
+    }
+
+    /**
+     * Check if a source is valid
+     *
+     * @param string $source The source to validate
+     *
+     * @return bool
+     */
+    private function isValidSource(string $source): bool
+    {
+        if (!class_exists($source)) {
+            return false;
+        }
+
+        $registrar = app(ConquestDataSourceRepository::class);
+        if (!$registrar->isRegistered($source)) {
+            return false;
+        }
+
+        try {
+            $class = new ReflectionClass($source);
+        } catch (ReflectionException $ex) {
+            return false;
+        }
+
+        return !$class->isAbstract() || !$class->implementsInterface(ConquestDataSource::class);
     }
 }
