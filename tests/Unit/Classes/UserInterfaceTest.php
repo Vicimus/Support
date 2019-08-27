a5713b411d34e2a0967c95215a21c143e2e873b4
Rob Houston <rhouston@vicimus.com>
2019-08-27 09:39:57 -0400

    Random coverage

---
+++ tests/Unit/Classes/UserInterfaceTest.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Tests\Unit\Classes;
+
+use PHPUnit\Framework\TestCase;
+use Vicimus\Support\Classes\UserInterface;
+
+/**
+ * Class TimerTest
+ */
+class UserInterfaceTest extends TestCase
+{
+    /**
+     * Assert extracting dependency node from file
+     * @return void
+     */
+    public function testDependancies(): void
+    {
+        $ui = new UserInterface();
+        $path = __DIR__ . '/../../../resources/testing/dependencies.json';
+        $dependencies = $ui->parseNodeDependencies($path);
+
+        $this->assertCount(1, $dependencies);
+        $this->assertArrayHasKey('one', $dependencies);
+        $this->assertSame(1, $dependencies['one']);
+
+        $public = $ui->publicDependencies(['public1' => 'one']);
+        $this->assertCount(1, $public);
+        $this->assertArrayHasKey('public1', $public);
+        $this->assertSame('one', $public['public1']);
+
+        $this->assertSame($public, $ui->public);
+    }
+}
