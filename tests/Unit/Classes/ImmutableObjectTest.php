8d61f8067a5ef8975f8ea105ff23d54768f610f4
Jordan Grieve <quintilex@gmail.com>
2019-09-13 08:58:05 -0400

    get some coverage

--- tests/Unit/Classes/ImmutableObjectTest.php
+++ tests/Unit/Classes/ImmutableObjectTest.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Validation\Factory;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Support\MessageBag;
 use InvalidArgumentException;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use stdClass;
 use Throwable;
@@ -51,7 +52,7 @@ class ImmutableObjectTest extends TestCase
         $obj = new ImmutableObject(['id' => 5]);
         $array = ['id' => 5];
 
-        $this->assertEquals($obj->__toString(), json_encode($array));
+        $this->assertEquals((string) $obj, json_encode($array));
     }
 
     /**
@@ -62,6 +63,7 @@ class ImmutableObjectTest extends TestCase
     public function testValidation(): void
     {
         $errors = '';
+        /** @var Factory|MockObject $factory */
         $factory = $this->getMockBuilder(Factory::class)
             ->getMock();
 
@@ -81,13 +83,13 @@ class ImmutableObjectTest extends TestCase
         try {
             $this->assertTrue($obj->isValid());
         } catch (Throwable $ex) {
-            $this->fail($ex->__toString());
+            $this->fail((string) $ex);
         }
 
         try {
             $errors = $obj->getValidationMessage();
         } catch (Throwable $ex) {
-            $this->fail($ex->__toString());
+            $this->fail((string) $ex);
         }
 
         $this->assertIsString($errors);
@@ -144,5 +146,40 @@ class ImmutableObjectTest extends TestCase
         $this->assertSame(1, $instance->id);
         $this->assertEquals('strawberry', $instance->other->banana);
         $this->assertEquals('kiwi', $instance->fruit);
+    }
+
+    /**
+     * Recursive Arrays
+     *
+     * @return void
+     */
+    public function testRecursiveArrays(): void
+    {
+        $instance = new ImmutableObject([
+            'object' => new ImmutableObject(['hi' => 'there']),
+            'children' => [
+                new ImmutableObject([
+                    'banana' => 'strawberry',
+                    'apples' => [1, 2, 3],
+                    'grand-children' => [
+                        new ImmutableObject(),
+                    ],
+                    'food' => [
+                        'fruit' => ['apple'],
+                    ],
+                ]),
+            ],
+            'banana' => 'strawberry',
+            'strawberry' => [
+                1, 2, 3,
+            ],
+        ]);
+
+        $result = $instance->toArray();
+        $this->assertIsArray($result);
+        $this->assertIsArray($result['children']);
+        $this->assertIsArray($result['children'][0]);
+        $this->assertIsArray($result['children'][0]['grand-children']);
+        $this->assertIsArray($result['children'][0]['grand-children'][0]);
     }
 }
