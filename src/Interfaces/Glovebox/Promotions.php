df5be600da2ffd8a11c028f538bc59be4b67b0e2
Jordan Grieve <quintilex@gmail.com>
2020-08-24 12:47:01 -0400

    promotions work (#238)

--- src/Interfaces/Glovebox/Promotions.php
+++ src/Interfaces/Glovebox/Promotions.php
@@ -4,6 +4,7 @@ namespace Vicimus\Support\Interfaces\Glovebox;
 
 use Illuminate\Database\Query\Builder as QBuilder;
 use Illuminate\Database\Eloquent\Builder;
+use Vicimus\Support\Interfaces\Glovebox\Promotions\Promotion;
 use Vicimus\Support\Interfaces\Glovebox\Promotions\PromotionIncentive;
 
 /**
@@ -11,6 +12,16 @@ use Vicimus\Support\Interfaces\Glovebox\Promotions\PromotionIncentive;
  */
 interface Promotions
 {
+    /**
+     * Find a promotion based on a vehicle id
+     *
+     * @param int      $vehicleId   The vehicle id
+     * @param int|null $promotionId Fallback promotion id
+     *
+     * @return Promotion|null
+     */
+    public function getByVehicleId($vehicleId, $promotionId = null);
+
     /**
      * Return the PromotionJoin query
      * @return Builder|QBuilder
