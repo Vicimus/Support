f9475db32b4b395ad5666ea633328d03a67596fe
Rob Houston <rhouston@vicimus.com>
2020-09-02 18:52:29 -0400

    typehints

--- src/Interfaces/MarketingSuite/CriteriaContract.php
+++ src/Interfaces/MarketingSuite/CriteriaContract.php
@@ -9,7 +9,15 @@ namespace Vicimus\Support\Interfaces\MarketingSuite;
  */
 interface CriteriaContract
 {
+    /**
+     * Retrieve the payload for setting the campaign criteria
+     * @return array
+     */
     public function payload(): array;
 
+    /**
+     * Retrieve the keywords for setting the ad group criteria
+     * @return array
+     */
     public function keywords(): array;
 }
