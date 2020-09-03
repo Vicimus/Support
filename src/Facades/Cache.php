5e20bb9e8e191e7653504e4abc0c92aafad07ec1
Jordan Grieve <quintilex@gmail.com>
2020-09-03 09:44:23 -0400

    more annotations

--- src/Facades/Cache.php
+++ src/Facades/Cache.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Cache as LaravelCache;
  *
  * @method static mixed remember(string $key, int|DateTimeInterface $ttl, callable $closure)
  * @method static mixed forget(string $key)
+ * @method static bool has(string $key)
+ * @method static void put(string $key, $value, int|DateTimeInterface $ttl)
+ * @method static mixed get(string $key)
  */
 class Cache extends LaravelCache
 {
