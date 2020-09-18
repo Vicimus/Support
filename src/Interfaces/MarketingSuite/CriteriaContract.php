444f0febf2f385865dc078045c67dc48c31e3988
Jordan Grieve <quintilex@gmail.com>
2020-09-18 07:49:33 -0400

    lint

--- src/Interfaces/MarketingSuite/CriteriaContract.php
+++ src/Interfaces/MarketingSuite/CriteriaContract.php
@@ -9,25 +9,25 @@ interface CriteriaContract
 {
     /**
      * Retrieve the payload for setting the campaign biddable group criteria
-     * @return array
+     * @return string[]|array
      */
     public function biddable(): array;
 
     /**
-     * Retrieve the payload for setting the campaign negative group criteria
-     * @return array
-     */
-    public function negative(): array;
-
-    /**
      * Retrieve the keywords for setting the ad group criteria
-     * @return array
+     * @return string[]|array
      */
     public function keywords(): array;
 
     /**
+     * Retrieve the payload for setting the campaign negative group criteria
+     * @return string[]|array
+     */
+    public function negative(): array;
+
+    /**
      * Retrieve the payload for setting the campaign criteria
-     * @return array
+     * @return string[]|array
      */
     public function payload(): array;
 }
