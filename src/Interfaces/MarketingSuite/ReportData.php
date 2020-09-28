4f19d45f52e83ded84e2c8ad51525135765f6335
Rob Houston <rhouston@vicimus.com>
2020-09-28 14:33:42 -0400

    report data interface

---
+++ src/Interfaces/MarketingSuite/ReportData.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vicimus\Support\Interfaces\MarketingSuite;
+
+/**
+ * Interface ReportData
+ */
+interface ReportData
+{
+    /**
+     * Implements toArray
+     *
+     * @return string[]
+     */
+    public function toArray(): array;
+}
