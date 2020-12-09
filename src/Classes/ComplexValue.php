24f512ef2f68cdc264e86c78e648bfbe93e45f17
Rob Houston <rhouston@vicimus.com>
2020-12-09 11:43:01 -0500

    interface changes

---
+++ src/Classes/ComplexValue.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vicimus\Support\Classes;
+
+use Illuminate\Contracts\Validation\Factory;
+
+/**
+ * Class ComplexValue
+ *
+ * Represents a select option on the front end which may provide additional data
+ */
+class ComplexValue extends ImmutableObject
+{
+    /**
+     * ComplexValue constructor
+     * 
+     * @param string|int|null $value      The value of the option
+     * @param string          $label      The display text of the option
+     * @param string|null     $additional Additioanl data to use in the display
+     */
+    public function __construct($value, string $label, ?string $additional = null)
+    {
+        parent::__construct([
+            'value' => $value,
+            'label' => $label,
+            'additional' => $additional,
+        ]);
+    }
+}
