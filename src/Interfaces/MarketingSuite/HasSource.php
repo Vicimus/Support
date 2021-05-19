e63c16a5b82263f529d2befc0aa8e08b06f888a9
Rob Houston <rhouston@vicimus.com>
2021-05-19 10:14:43 -0400

    good touch

--- src/Interfaces/MarketingSuite/HasSource.php
+++ src/Interfaces/MarketingSuite/HasSource.php
@@ -3,8 +3,9 @@
 namespace Vicimus\Support\Interfaces\MarketingSuite;
 
 use Assets\Models\Source;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Vicimus\Support\Database\Model;
 
 /**
  * Interface HasSource
@@ -21,21 +22,25 @@ interface HasSource
 
     /**
      * Has many audiences
-     *
      * @return MorphMany
      */
     public function audiences(): MorphMany;
 
     /**
      * Retrieve the related source
-     * @return Source|BelongsTo|null
+     * @return MorphOne|Source
      */
     public function source();
 
     /**
      * Get the store id for this campaigns
-     *
      * @return int
      */
     public function storeId(): int;
+
+    /**
+     * Update the models timestamp
+     * @return bool
+     */
+    public function touch();
 }
