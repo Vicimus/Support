d4637bbb7ff4b9a6e7920e42368cdbcbc1a37c49
Rob Houston <rhouston@vicimus.com>
2021-05-20 06:55:39 -0400

    dev(shared): has source interface, source has sourceable (#270)
    Source is now polymorphic and a 1:1 a opposed to 1:*. Adds the HasSource interface for conquest/intentory campaigns.
    
    https://vicimus.atlassian.net/browse/BUMP-7210

---
+++ src/Interfaces/MarketingSuite/HasSource.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces\MarketingSuite;
+
+use Assets\Models\Source;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+/**
+ * Interface HasSource
+ *
+ *
+ */
+interface HasSource
+{
+    /**
+     * Has many assets
+     * @return MorphMany|AssetModel[]
+     */
+    public function assets(): MorphMany;
+
+    /**
+     * Has many audiences
+     * @return MorphMany
+     */
+    public function audiences(): MorphMany;
+
+    /**
+     * Retrieve the related source
+     * @return MorphOne|Source
+     */
+    public function source();
+
+    /**
+     * Get the store id for this campaigns
+     * @return int
+     */
+    public function storeId(): int;
+
+    /**
+     * Update the models timestamp
+     * @return bool
+     */
+    public function touch();
+}
--- src/Interfaces/MarketingSuite/SourceRecord.php
+++ src/Interfaces/MarketingSuite/SourceRecord.php
@@ -2,8 +2,8 @@
 
 namespace Vicimus\Support\Interfaces\MarketingSuite;
 
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Collection;
 
 /**
@@ -20,13 +20,6 @@ interface SourceRecord
      * @return MorphMany
      */
     public function audiences(): MorphMany;
-
-    /**
-     * A source belongs to a campaign
-     *
-     * @return BelongsTo|Campaign
-     */
-    public function campaign(): BelongsTo;
 
     /**
      * Get an instance of the implementation
@@ -53,6 +46,13 @@ interface SourceRecord
      * @return void
      */
     public function record(string $property, $value = null): void;
+
+    /**
+     * A source belongs to a campaign
+     *
+     * @return MorphTo|HasSource
+     */
+    public function sourceable(): MorphTo;
 
     /**
      * Get the store id this source is associated with
