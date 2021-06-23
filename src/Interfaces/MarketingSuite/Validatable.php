f626ebf1d1fbd8fb0487469dfb3f019eac49e988
Rob Houston <rhouston@vicimus.com>
2021-06-23 14:44:16 -0400

    validatable

---
+++ src/Interfaces/MarketingSuite/Validatable.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vicimus\Support\Interfaces\MarketingSuite;
+
+use Vicimus\Support\Interfaces\MarketingSuite\Asset;
+
+/**
+ * Interface Validatable
+ *
+ * @property string $title
+ */
+interface Validatable
+{
+    /**
+     * Retrieve the lead type id assigned to the campaign
+     *
+     * @return int|null
+     */
+    public function leadTypeId(): ?int;
+
+    /**
+     * This method should return if the campaign is utilizing a specific
+     * medium. Is it sending letters, sending emails, using facebook carousel,
+     * etc.
+     *
+     * @param string $slug The asset type slug
+     *
+     * @return bool
+     */
+    public function medium(string $slug): bool;
+
+    /**
+     * Get the portfolio of assets for this campaign
+     *
+     * @return Asset[]
+     */
+    public function portfolio(): array;
+
+    /**
+     * Get the store id for this campaigns
+     *
+     * @return int
+     */
+    public function storeId(): int;
+}
