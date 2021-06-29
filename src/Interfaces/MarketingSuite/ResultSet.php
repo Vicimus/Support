16a01c56a5d6ed3b0e7ccce10764832b3b3aa61e
Rob Houston <rhouston@vicimus.com>
2021-06-29 14:45:33 -0400

    dev(inventory-ads): result set interface (#285)

---
+++ src/Interfaces/MarketingSuite/ResultSet.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Interface ResultSet
+ */
+interface ResultSet
+{
+    /**
+     * Get the rows
+     * @return Collection
+     */
+    public function getRows(): Collection;
+}
