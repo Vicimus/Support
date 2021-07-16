8ca1012f7cf90ab95932a32011f627cb6c4c91f7
Jordan Grieve <quintilex@gmail.com>
2021-07-16 09:23:19 -0400

    github actions and phpcs fix

--- src/Interfaces/MarketingSuite/ConquestDataSourceVerificationResponse.php
+++ src/Interfaces/MarketingSuite/ConquestDataSourceVerificationResponse.php
@@ -1,7 +1,10 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace Vicimus\Support\Interfaces\MarketingSuite;
 
+/**
+ * Interface ConquestDataSourceVerificationResponse
+ */
 interface ConquestDataSourceVerificationResponse
 {
     /**
