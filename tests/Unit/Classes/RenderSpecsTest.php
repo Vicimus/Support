d0c35344dde9c01b157190c8f77e7f85d7ae9f81
Rob Houston <rhouston@vicimus.com>
2021-07-22 08:30:37 -0400

    actual coverage

--- tests/Unit/Classes/RenderSpecsTest.php
+++ tests/Unit/Classes/RenderSpecsTest.php
@@ -2,6 +2,7 @@
 
 namespace Vicimus\Support\Tests\Unit\Classes;
 
+use Illuminate\Http\Request;
 use Vicimus\Support\Classes\RenderSpecs;
 use Vicimus\Support\Testing\TestCase;
 
@@ -18,6 +19,31 @@ class RenderSpecsTest extends TestCase
     public function testConstructor(): void
     {
         $render = new RenderSpecs(1, 2, 3, 4, false, true);
+        $this->assertEquals(1, $render->width);
+        $this->assertEquals(2, $render->height);
+        $this->assertEquals(3, $render->scale);
+        $this->assertEquals(4, $render->pages);
+        $this->assertFalse($render->letter);
+        $this->assertTrue($render->postcard);
+    }
+
+    /**
+     * Constructor should assign properties properly from a request object
+     *
+     * @return void
+     */
+    public function testConstructorWithRequest(): void
+    {
+        $request = new Request([
+            'width' => 1,
+            'height' => 2,
+            'scale' => 3,
+            'pages' => 4,
+            'letter' => false,
+            'postcard' => true,
+        ]);
+
+        $render = new RenderSpecs($request);
         $this->assertEquals(1, $render->width);
         $this->assertEquals(2, $render->height);
         $this->assertEquals(3, $render->scale);
