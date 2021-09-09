28e1a0cd620e289fe80b8d212f0d5ac5b0a5db31
Rob Houston <rhouston@vicimus.com>
2021-09-09 14:01:08 -0400

    appointment contract

---
+++ src/Interfaces/MarketingSuite/AppointmentContract.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces\MarketingSuite;
+
+/**
+ * Interface AppointmentContract
+ *
+ * @property Carbon $date
+ */
+interface AppointmentContract
+{
+    //
+}
