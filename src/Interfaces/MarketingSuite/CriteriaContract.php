37645230cab5f0d4c93b303bfca1e45be225d2c4
Rob Houston <rhouston@vicimus.com>
2021-11-11 17:02:24 -0500

    dont need these

--- src/Interfaces/MarketingSuite/CriteriaContract.php
+++ src/Interfaces/MarketingSuite/CriteriaContract.php
@@ -24,16 +24,4 @@ interface CriteriaContract
      * @return string[]|array
      */
     public function keywords(): array;
-
-    /**
-     * Retrieve the payload for setting the campaign negative group criteria
-     * @return string[]|array
-     */
-    public function negative(): array;
-
-    /**
-     * Retrieve the payload for setting the campaign criteria
-     * @return string[]|array
-     */
-    public function payload(): array;
 }
