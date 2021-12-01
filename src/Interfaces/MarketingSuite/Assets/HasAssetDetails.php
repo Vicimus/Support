9a24fb46c0ed128b5b539d0b2c23cc6c65700ece
Rob Houston <rhouston@vicimus.com>
2021-12-01 12:10:07 -0500

    dev(shared): asset details interfaces
    Adds an interface for retrieving a model which contains remote details on a placement. Adds a method to HasSource to retrieve this interface.
    
    https://vicimus.atlassian.net/browse/BUMP-8202

---
+++ src/Interfaces/MarketingSuite/Assets/HasAssetDetails.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces\MarketingSuite\Assets;
+
+interface HasAssetDetails
+{
+    /**
+     * Asset errored with the provided payload
+     *
+     * @param string $message The error message
+     *
+     * @return void
+     */
+    public function error(string $message): void;
+}
