1975a0146c0ae7309810309956407195f47a9005
Andrew Benner <abenner@vicimus.com>
2022-11-02 10:46:40 -0400

    Result interface never existed for ThemeManager interface

---
+++ src/Interfaces/Glovebox/Result.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces\Glovebox;
+
+/**
+ * Represents a result from a theme refresh
+ */
+interface Result
+{
+    /**
+     * Check if the command failed or not
+     *
+     * @return bool
+     */
+    public function failed(): bool;
+
+    /**
+     * Check if the command succeeded
+     *
+     * @return bool
+     */
+    public function success(): bool;
+
+    /**
+     * Get the output from the command
+     *
+     * @return array
+     */
+    public function output(): bool;
+}
