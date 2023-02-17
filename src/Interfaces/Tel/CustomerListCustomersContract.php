48efb15ddfac14d24db908de71db96880edc1e69
Robert Houston <rhouston@vicimus.com>
2023-02-17 16:07:35 -0500

    cleanup

--- src/Interfaces/Tel/CustomerListCustomersContract.php
+++ src/Interfaces/Tel/CustomerListCustomersContract.php
@@ -2,7 +2,6 @@
 
 namespace Vicimus\Support\Interfaces\Tel;
 
-use Carbon\Carbon;
 use GuzzleHttp\Exception\GuzzleException;
 
 /**
@@ -13,9 +12,9 @@ interface CustomerListCustomersContract
     /**
      * Update the customer list for a campaign
      *
-     * @param int   $storeId    The store making the request
-     * @param int   $prospectId The prospect id of the associated customer
-     * @param string[] $data    The data to update with
+     * @param int      $storeId    The store making the request
+     * @param int      $prospectId The prospect id of the associated customer
+     * @param string[] $data       The data to update with
      *
      * @return CustomerListContract
      * @throws GuzzleException
