22ad662dbd46034dd16959e8e311ae6a8bbdec12
Rob Houston <rhouston@vicimus.com>
2023-02-21 10:01:23 -0500

    dev(shared): customer list customer service (#365)
    Adds an interface for interacting with customer list customers.

---
+++ src/Interfaces/Tel/CustomerListCustomersContract.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces\Tel;
+
+use GuzzleHttp\Exception\GuzzleException;
+
+/**
+ * Customer list customer service contract
+ */
+interface CustomerListCustomersContract
+{
+    /**
+     * Update the customer list for a campaign
+     *
+     * @param int      $storeId    The store making the request
+     * @param int      $prospectId The prospect id of the associated customer
+     * @param string[] $data       The data to update with
+     *
+     * @return CustomerListContract
+     * @throws GuzzleException
+     */
+    public function update(
+        int $storeId,
+        int $prospectId,
+        array $data
+    ): void;
+}
