661f0a9d71a45647f62b0f2b0a3bce987d5972b3
Jordan Grieve <quintilex@gmail.com>
2023-08-18 10:37:45 -0400

    more accurate annotations

--- src/Interfaces/MarketingSuite/Campaign.php
+++ src/Interfaces/MarketingSuite/Campaign.php
@@ -26,9 +26,9 @@ use Vicimus\Support\Interfaces\Eloquent;
  * @property Carbon $created_at
  * @property string $live_site
  * @property string $purl_background
- * @property bool $send_email
- * @property bool $send_letter
- * @property bool $send_voice
+ * @property bool|int $send_email
+ * @property bool|int $send_letter
+ * @property bool|int $send_voice
  * @property ScriptContract $script
  * @property mixed $letter
  * @property mixed $postcard
@@ -36,11 +36,11 @@ use Vicimus\Support\Interfaces\Eloquent;
  * @property Carbon $email_at
  * @property Carbon $print_at
  * @property bool $send_optimization
- * @property bool $send_bdc
- * @property bool $send_sms
+ * @property bool|int $send_bdc
+ * @property bool|int $send_sms
  * @property Carbon $print_request
  * @property Carbon $sms_at
- * @property bool $send_postcard
+ * @property bool|int $send_postcard
  */
 interface Campaign extends Eloquent
 {
