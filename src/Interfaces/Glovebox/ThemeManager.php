614599836eabd11558c07150a29d248c74db8dce
Andrew Benner <abenner@vicimus.com>
2023-10-18 09:22:13 -0400

    fix(gbx): add command param to theme manager (#377)
    Needed to add the $command parameter to the ThemeManager interface for
    gbx.
    
    https://vicimus.atlassian.net/browse/GBX-6206

--- src/Interfaces/Glovebox/ThemeManager.php
+++ src/Interfaces/Glovebox/ThemeManager.php
@@ -10,7 +10,9 @@ interface ThemeManager
     /**
      * Run Grunt Web in the Eevee directory to recompile the theme scss
      *
+     * @param string $command The command to run
+     *
      * @return Result
      */
-    public function refresh(): Result;
+    public function refresh(string $command): Result;
 }
