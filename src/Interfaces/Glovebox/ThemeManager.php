4571c57bdbd402aab07a8a4347c2d8690fc6c267
Andrew Benner <abenner@vicimus.com>
2023-10-18 09:12:37 -0400

    need doc

--- src/Interfaces/Glovebox/ThemeManager.php
+++ src/Interfaces/Glovebox/ThemeManager.php
@@ -10,6 +10,8 @@ interface ThemeManager
     /**
      * Run Grunt Web in the Eevee directory to recompile the theme scss
      *
+     * @param string $command The command to run
+     *
      * @return Result
      */
     public function refresh(string $command): Result;
