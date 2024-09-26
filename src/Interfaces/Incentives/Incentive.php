a22e510b184350abb5d5aeb34a79888deb534080
Jordan Grieve <quintilex@gmail.com>
2024-09-26 12:06:16 -0400

    interfaces

--- src/Interfaces/Incentives/Incentive.php
+++ src/Interfaces/Incentives/Incentive.php
@@ -2,10 +2,53 @@
 
 namespace Vicimus\Support\Interfaces\Incentives;
 
-/**
- * Incentive
- */
 interface Incentive
 {
-    //
+    /**
+     * Does this incentive have a lease rate
+     * @return bool
+     */
+    public function hasLeaseRate(): bool;
+
+    /**
+     * Does this incentive have a finance rate
+     * @return bool
+     */
+    public function hasRate(): bool;
+
+    /**
+     * Get the highest rate in the program
+     * @return Term
+     */
+    public function highestRate(): Term;
+
+    /**
+     * Get the highest term in the program
+     * @return Term
+     */
+    public function highestTerm(): Term;
+
+    /**
+     * Is this residuals
+     * @return bool
+     */
+    public function isResiduals(): bool;
+
+    /**
+     * Get the institution
+     * @return Institution
+     */
+    public function institution(): Institution;
+
+    /**
+     * Get the lowest rate in the program
+     * @return Term
+     */
+    public function lowestRate(): Term;
+
+    /**
+     * Get the lowest term in the program
+     * @return Term
+     */
+    public function lowestTerm(): Term;
 }
