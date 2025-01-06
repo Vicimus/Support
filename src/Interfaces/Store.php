eeaf25db553b9f9e9b5a8f783b06a97394038fc0
Jordan Grieve <quintilex@gmail.com>
2025-01-06 16:09:55 -0500

    Revert "required method on store interface"
    This reverts commit f7d184c77833831d7fa1968c1f7673f914306301.

--- src/Interfaces/Store.php
+++ src/Interfaces/Store.php
@@ -13,15 +13,6 @@ namespace Vicimus\Support\Interfaces;
 interface Store
 {
     /**
-     * Determine if a store has a property or not
-     *
-     * @param string $property The property to check
-     *
-     * @return bool
-     */
-    public function hasProperty(string $property): bool;
-
-    /**
      * Get the primary identifier for the store
      */
     public function identifier(): string | int;
