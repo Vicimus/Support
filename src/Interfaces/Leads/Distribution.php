6d6ed1f51ca9c49bc2a16e320af8bddf8d934c9d
Jordan Grieve <quintilex@gmail.com>
2025-02-06 09:26:19 -0500

    dev(shared): distribution interface (#400)
    This is to support advanced lead and adf features.
    https://vicimus.atlassian.net/browse/BUMP-12367

---
+++ src/Interfaces/Leads/Distribution.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types = 1);
+
+namespace Vicimus\Support\Interfaces\Leads;
+
+/**
+ * Represent a lead distribution with configuration information
+ */
+interface Distribution
+{
+    /**
+     * @return string
+     */
+    public function getHttpAdfFormat(): string;
+
+    /**
+     * @return string
+     */
+    public function getHttpProviderId(): string;
+
+    /**
+     * @return string
+     */
+    public function getHttpVendorId(): string;
+}
