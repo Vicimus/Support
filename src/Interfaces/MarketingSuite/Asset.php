b8a014654eca548aed0340cad2e3a10e602ba861
Jordan Grieve <quintilex@gmail.com>
2026-01-06 10:59:29 -0500

    adjusts the asset contract to be useful

--- src/Interfaces/MarketingSuite/Asset.php
+++ src/Interfaces/MarketingSuite/Asset.php
@@ -4,89 +4,11 @@ declare(strict_types=1);
 
 namespace Vicimus\Support\Interfaces\MarketingSuite;
 
-use Vicimus\Support\Interfaces\Marketingsuite\Assets\CallToAction;
-use Vicimus\Support\Interfaces\MarketingSuite\Assets\Instruction;
-use Vicimus\Support\Interfaces\MarketingSuite\Assets\Validator;
-use Vicimus\Support\Interfaces\MarketingSuite\Exceptions\TemplateException;
-
 /**
- * Interface Asset
- *
  * @property string $template
- * @phpcsSuppress SlevomatCodingStandard.TypeHints.DisallowMixedTypeHint
- * @property mixed[] $tabs
+ * @property (string | int)[][] $tabs
  */
 interface Asset extends AssetContract
 {
-    /**
-     * Determine if an asset has been approved
-     *
-     */
-    public function active(): bool;
-
-    /**
-     * Determine if an asset has been approved
-     *
-     */
-    public function approved(): bool;
-
-    /**
-     * Retrieve the call to actions for the asset
-     * @return CallToAction[]
-     */
-    public function callToActions(): array;
-
-    /**
-     * Get the height
-     */
-    public function height(): int;
-
-    /**
-     * Retrieve the instructions on the asset. This is not always populated
-     * and generally is only populated when necessary and sending to the
-     * PURL application
-     *
-     * @return Instruction[]
-     */
-    public function instructions(): array;
-
-    /**
-     * The intent of the Asset, conquest|retention
-     */
-    public function intent(): string;
-
-    /**
-     * Get the rendered markup
-     */
-    public function rendered(): string;
-
-    public function structureId(): string | int | null;
-
-    /**
-     * Convert the asset into an array that is easy to send over the network
-     *
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.DisallowMixedTypeHint
-     * @return mixed[]
-     */
-    public function toArray(): array;
-
-    /**
-     * Get the slug or type of asset this represents
-     */
-    public function type(): string;
-
-    /**
-     * Validate an asset
-     *
-     * @param string[]       $errors    The errors found
-     * @param Validator|null $validator Optionally provide a validator to use
-     *
-     * @throws TemplateException
-     */
-    public function validate(array &$errors = [], ?Validator $validator = null): bool;
-
-    /**
-     * The width of this asset
-     */
-    public function width(): int;
+    //
 }
